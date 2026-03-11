9c009cfd84d18eab5f9b40682afd88a4b0089a67
Marija Andric <marijaandric2001@gmail.com>
2026-03-12 00:53:50 +0100

    Change curl into apt

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -9,19 +9,19 @@ vi.mock('/vite.svg', () => ({ default: 'vite.svg' }), { virtual: true })
 
 
 describe('App', () => {
-  // test('renders heading', () => {
-  //   render(<App />)
+  test('renders heading', () => {
+    render(<App />)
 
-  //   expect(screen.getByRole('heading', { name: /vite \+ react/i })).toBeTruthy()
-  // })
+    expect(screen.getByRole('heading', { name: /vite \+ react/i })).toBeTruthy()
+  })
 
-  // test('increments counter on button click', async () => {
-  //   const user = userEvent.setup()
-  //   render(<App />)
+  test('increments counter on button click', async () => {
+    const user = userEvent.setup()
+    render(<App />)
 
-  //   const button = screen.getAllByRole('button', { name: /count is 0/i })[0]
-  //   await user.click(button)
+    const button = screen.getAllByRole('button', { name: /count is 0/i })[0]
+    await user.click(button)
 
-  //   expect(screen.getAllByRole('button', { name: /count is 1/i })[0]).toBeTruthy()
-  // })
+    expect(screen.getAllByRole('button', { name: /count is 1/i })[0]).toBeTruthy()
+  })
 })
